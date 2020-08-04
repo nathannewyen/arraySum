1fc5cb42173ca33b95248ec3377749e23af71f34
Nhan Nguyen <nathan13574@gmail.com>
2020-08-04 18:15:59 -0500

    commit

---
+++ arrSum.js
@@ -0,0 +1,11 @@
+arr = [1, 2, 3]
+
+function arraySum(ar) {
+    const sum = 0
+    for (let i = 0; i < ar.length; i++) {
+        sum += ar[i]
+    }
+    return sum
+}
+
+console.log(arraySum(arr))
